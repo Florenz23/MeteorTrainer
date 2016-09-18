8ab6d6f9eb0ad67a917dc619ad3e0f93ea569658
lama23 <florenz.erstling@lalalama.de>
2016-09-18 11:50:38 +0300

    CsvToJsonConverter  It is working now, some little changes must be made

--- imports/ui/components/fcList/csvListAdd/csvListAdd.js
+++ imports/ui/components/fcList/csvListAdd/csvListAdd.js
@@ -1,11 +1,11 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import uiRouter from 'angular-ui-router';
-import { Meteor } from 'meteor/meteor';
 
 import template from './csvListAdd.html';
-import { Parties } from '../../../../api/fcLists';
-import { FlashCards } from '../../../../api/flashCards';
+import {Parties} from '../../../../api/fcLists';
+import {FlashCards} from '../../../../api/flashCards';
+import {CsvJsonConverter} from './csvConvertFactory'
 
 class CsvAdd {
     constructor($stateParams) {
@@ -13,30 +13,52 @@ class CsvAdd {
         this.fcList = {};
         this.list = {};
         this.fcList.listId = "WEEk8A8NWaKdNRNMJ";
-/*      this.test = { "_id" : "4qvSuXqFiSQw4WC7z", "question" : "я", "answer" : "ja", "owner" : "sz4ptdPRdaYvpCYfe", "listId" : "5DPjftxrGTdQizWQw", "right" : 4, "wrong" : 0, "rating" : 4, "lastRevision" : 1467230430219, "importance" : -26.34075476606216 };*/
+        this.data = {};
+        this.data.csv = '"ja","yes","eng"\n"nein","no","eng"';
+        this.data.json = "moin";
+        this.data.keys = '"question","answer","comment","owner","listId","right","wrong","rating","lastRevision","importance"';
+        this.data.listId = "WEEk8A8NWaKdNRNMJ";
+        this.data.importance = "not learned";
+    }
+
+    getKeys = function () {
+        var csv = this.data.keys;
+        var array = CsvJsonConverter.CSVToArray(csv);
+        return array;
+    };
+
+    convertToJson = function () {
+        var convert_data = {};
+        convert_data.csv = this.data.csv;
+        convert_data.key_arr = this.getKeys();
+        convert_data.owner = Meteor.user()._id;
+        convert_data.listId = this.data.listId;
+        var json = CsvJsonConverter.CSV2JSON(convert_data);
+        console.log(json);
+        return json;
+    };
+    display = function () {
+        var owner = "asdflkj23234";
+        var json = this.convertToJson(owner);
+        this.data.json = json;
     }
 
     submit() {
-        this.fcList.owner = Meteor.user()._id;
-        this.test = {  "question" : "я", "answer" : "ja", "owner" : Meteor.user()._id, "listId" : this.fcList.listId, "right" : 0, "wrong" : 0, "rating" : 0, "lastRevision" : "notLearned" };
-        console.log(this.test);
-        FlashCards.insert(this.test, function(err,docsInserted){
-            if(err)console.log(err);
-            else {
-                // The above statement will output the id of the
-                // inserted object
-            }
-        });
+        var data = this.convertToJson();
+        data = JSON.parse(data);
+        for (var i = 0;i<data.length;i++){
+            FlashCards.insert(data[i]);
+        }
         this.reset();
 
-        if(this.done) {
+        if (this.done) {
             this.done();
         }
 
     }
 
     reset() {
-        this.fcList = {};
+        this.data = {};
     }
 }
 
